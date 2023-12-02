58db2908db6139893e89a7b55d896a8cf64a80e5
Biplob Sarkar <ramjansarkar3899@gmail.com>
2023-12-03 04:36:56 +0600

    do..while loop implement

--- controlFlow/loops/javaScriptLoops.js
+++ controlFlow/loops/javaScriptLoops.js
@@ -6,7 +6,7 @@
 	statment 
 }*/
 
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 5; i++) {
 	//console.log('Hello Biplob', i);
 	if (i % 2 !==0) console.log(i);
 } 
@@ -15,7 +15,15 @@ for (let i = 1; i <= 100; i++) {
 // while loop
 let i = 0;
 while (i<=5) {
-	if (1 % 2 !== 0)  console.log(i);
-	i++;
-	
+	if (i % 2 !== 0)  console.log(i);
+	i++;	
 }
+
+				// Do...while loop
+// the statement execute at list one time , if the conditions is false 
+let index = 11;
+do {
+	if (index % 2 !== 0)  console.log(index);
+	index++;
+} while (index<=5); //Outputs: 11
+
