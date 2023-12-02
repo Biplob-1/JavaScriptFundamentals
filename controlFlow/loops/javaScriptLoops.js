9f74681610c714dffc8b96e1ee5637e17c73ff8c
Biplob Sarkar <ramjansarkar3899@gmail.com>
2023-12-03 04:22:10 +0600

    while loop aded

--- controlFlow/loops/javaScriptLoops.js
+++ controlFlow/loops/javaScriptLoops.js
@@ -10,3 +10,12 @@ for (let i = 1; i <= 100; i++) {
 	//console.log('Hello Biplob', i);
 	if (i % 2 !==0) console.log(i);
 } 
+
+
+// while loop
+let i = 0;
+while (i<=5) {
+	if (1 % 2 !== 0)  console.log(i);
+	i++;
+	
+}
